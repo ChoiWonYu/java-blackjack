d25751509b55886884ae351918b16d910df2d78e
WonyuChoi <phillip07290@naver.com>
2023-10-06 21:29:46 +0900

    refactor: 원시값 포장한 Name으로 인해 정적 팩토리 메서드 수정

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -2,18 +2,19 @@ package domain;
 
 public class Dealer extends Player {
 
-    private final int STANDARD_OF_ADDITIONAl_CARD=17;
+    private final int STANDARD_OF_ADDITIONAl_CARD = 17;
 
-    private Dealer(String name,Deck deck) {
+    private Dealer(Name name, Deck deck) {
         super(name, deck);
     }
 
     public static Dealer createDefaultDealer(final String name) {
-        Deck initialDeck=Deck.createDefaultDeck();
-        return new Dealer(name, initialDeck);
+        Deck initialDeck = Deck.createDefaultDeck();
+        Name initialName = new Name(name);
+        return new Dealer(initialName, initialDeck);
     }
 
     public boolean haveToPickMoreCard() {
-        return this.getCardSum()>=STANDARD_OF_ADDITIONAl_CARD;
+        return this.getCardSum() >= STANDARD_OF_ADDITIONAl_CARD;
     }
 }
