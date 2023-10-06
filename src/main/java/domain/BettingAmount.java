d9993d4cff15f6cf4bf2bdfb0b58274faf725d1e
WonyuChoi <phillip07290@naver.com>
2023-10-06 21:26:26 +0900

    feat: 배팅금 원시값 포장 객체

---
+++ src/main/java/domain/BettingAmount.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class BettingAmount {
+
+    private final int bettingAmount;
+
+    public BettingAmount(int bettingAmount) {
+        this.bettingAmount=bettingAmount;
+    }
+}
