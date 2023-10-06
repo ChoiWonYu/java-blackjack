7e3ea68e8fb346c20c8d4c5b7b2f59453c916222
WonyuChoi <phillip07290@naver.com>
2023-10-06 21:31:17 +0900

    feat: player의 정보를 dto로 변환하는 메서드 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,41 +1,67 @@
 package domain;
 
+import controller.dto.PlayerDeck;
 import java.util.List;
 
 public class Player {
 
-    protected final int MAX_NUMBER=21;
+    protected final int MAX_NUMBER = 21;
 
-    protected final String name;
+    protected final Name name;
     protected final Deck deck;
+    private BettingAmount bettingAmount;
 
+    protected Player(Name name, Deck deck) {
+        this.name = name;
+        this.deck = deck;
+    }
 
-    protected Player(String name,Deck deck) {
-        this.name=name;
-        this.deck=deck;
+    private Player(Name name, Deck deck, BettingAmount bettingAmount) {
+        this.name = name;
+        this.deck = deck;
+        this.bettingAmount = bettingAmount;
     }
 
 
-    public static Player createDefault(final String name) {
-        Deck initialDeck=Deck.createDefaultDeck();
-        return new Player(name,initialDeck);
+    public static Player createDefault(final String name, final int amount) {
+        Deck initialDeck = Deck.createDefaultDeck();
+        Name playersName = new Name(name);
+        BettingAmount bettingAmount = new BettingAmount(amount);
+        return new Player(playersName, initialDeck, bettingAmount);
     }
 
-    public void addCardToDeck(final List<Card> firstCards) {
+    public void addCardsToDeck(final List<Card> firstCards) {
         deck.addCards(firstCards);
+    }
+
+    public void addCardToDeck(final Card card) {
+        deck.addCard(card);
     }
 
     public boolean isBlackJack() {
         int deckSum = deck.getSum();
-        return deckSum==MAX_NUMBER;
+        return deckSum == MAX_NUMBER;
     }
 
     public boolean isBurst() {
-        int deckSum=deck.getSum();
+        int deckSum = deck.getSum();
         return deckSum > MAX_NUMBER;
     }
 
     public int getCardSum() {
         return deck.getSum();
+    }
+
+    public String getNameValue() {
+        return name.getName();
+    }
+
+    public List<String> getCardInfos() {
+        return deck.getCardInfo();
+    }
+
+    public PlayerDeck toCommonDto() {
+        List<String> cardInfos = getCardInfos();
+        return new PlayerDeck(getNameValue(), String.join(", ", cardInfos));
     }
 }
