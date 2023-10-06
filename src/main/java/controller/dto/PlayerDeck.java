9bdafb79cb2a1b1e716c5c6254a8e8b5e5eae076
WonyuChoi <phillip07290@naver.com>
2023-10-06 21:22:56 +0900

    feat: player의 이름, 카드들을 출력할 때 이용할 dto 객체

---
+++ src/main/java/controller/dto/PlayerDeck.java
@@ -0,0 +1,19 @@
+package controller.dto;
+
+public class PlayerDeck {
+    protected final String name;
+    protected final String cards;
+
+    public PlayerDeck(String name,String cards) {
+        this.cards=cards;
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCards() {
+        return cards;
+    }
+}
