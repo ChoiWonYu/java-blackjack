6deaec1a4bab995549873d0127e2733b61b88e35
WonyuChoi <phillip07290@naver.com>
2023-10-06 15:26:07 +0900

    feat: card 역할을 하는 객체

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Card {
+
+    private final int number;
+    private final String shape;
+    public Card(final int number, final String shape) {
+        this.number=number;
+        this.shape=shape;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
