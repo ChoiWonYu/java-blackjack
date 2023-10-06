c9893fc6ce9bac6dc3a4146ebf1fe07ea37f5948
WonyuChoi <phillip07290@naver.com>
2023-10-06 21:24:11 +0900

    feat: 이름을 원시값 포장한 객체

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
