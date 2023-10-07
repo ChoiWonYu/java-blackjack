211d2a6cc3e11b8bff283220ae5075a83971e2b3
WonyuChoi <phillip07290@naver.com>
2023-10-07 15:47:43 +0900

    feat: 수익금 계산 객체를 멤버로 받고, 수익금을 계산하고 dto로 반환하는 메서드 추가

--- src/main/java/service/BlackJackGame.java
+++ src/main/java/service/BlackJackGame.java
@@ -2,6 +2,7 @@ package service;
 
 import controller.dto.PlayerDeck;
 import controller.dto.PlayerDeckResult;
+import controller.dto.PlayerRevenue;
 import domain.Card;
 import domain.Cards;
 import domain.Dealer;
@@ -12,38 +13,43 @@ import java.util.List;
 
 public class BlackJackGame {
 
-    private final int INITIAL_CARDS_COUNT=2;
+    private final int INITIAL_CARDS_COUNT = 2;
 
     private final Dealer dealer;
     private final Players players;
     private final Cards cards;
+    private final RevenueCalculator calculator;
 
-    private BlackJackGame(Dealer dealer,Players players,Cards cards) {
-        this.dealer=dealer;
-        this.players=players;
-        this.cards=cards;
+    private BlackJackGame(Dealer dealer, Players players, Cards cards,
+        RevenueCalculator calculator) {
+        this.dealer = dealer;
+        this.players = players;
+        this.cards = cards;
+        this.calculator = calculator;
     }
 
     public static BlackJackGame createDefaultGame() {
-        Dealer defaultDealer = Dealer.createDefaultDealer("딜러");
+        Dealer defaultDealer = Dealer.createDefaultDealer();
         Cards cards = Cards.createDefaultCards();
         Players players = Players.createInitialPlayers(new ArrayList<>());
-        return new BlackJackGame(defaultDealer, players,cards);
+        RevenueCalculator revenueCalculator = RevenueCalculator.createCalculatorWithoutPlayer(
+            defaultDealer);
+        return new BlackJackGame(defaultDealer, players, cards, revenueCalculator);
     }
 
     public void firstDealOutCards() {
         List<Card> dealerCards = pickCards(INITIAL_CARDS_COUNT);
         dealer.addCardsToDeck(dealerCards);
 
-        players.actEachPlayer((player)-> {
+        players.actEachPlayer((player) -> {
             List<Card> pickedCards = pickCards(INITIAL_CARDS_COUNT);
             player.addCardsToDeck(pickedCards);
         });
     }
 
     public PlayerDeck giveCardToPlayer(String playerName) {
-        Card pickedCard=pickCard();
-        Player player=players.getPlayerByName(playerName);
+        Card pickedCard = pickCard();
+        Player player = players.getPlayerByName(playerName);
         player.addCardToDeck(pickedCard);
         return player.toCommonDto();
     }
@@ -74,7 +80,7 @@ public class BlackJackGame {
 
     public boolean canPlayerContinueGame(final String name) {
         Player player = players.getPlayerByName(name);
-        if(player.isBurst()){
+        if (player.isBurst()) {
             return false;
         }
 
@@ -86,7 +92,7 @@ public class BlackJackGame {
     }
 
     public PlayerDeck giveDealerMoreCard() {
-        Card pickedCard=pickCard();
+        Card pickedCard = pickCard();
         dealer.addCardToDeck(pickedCard);
         return dealer.toCommonDto();
     }
@@ -97,5 +103,16 @@ public class BlackJackGame {
 
     public List<PlayerDeckResult> getAllPlayerResults() {
         return players.getPlayerResults();
+    }
+
+    public void calculateRevenue() {
+        players.actEachPlayer(calculator::calculateRevenue);
+    }
+
+    public List<PlayerRevenue> getPlayersRevenue() {
+        PlayerRevenue dealerRevenue=dealer.toRevenueDto();
+        List<PlayerRevenue> playerRevenues=players.getPlayerRevenueResults();
+        playerRevenues.add(0,dealerRevenue);
+        return new ArrayList<>(playerRevenues);
     }
 }
