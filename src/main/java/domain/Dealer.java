57aab5030727b1a898eb818cc0c098cf413d70f8
WonyuChoi <phillip07290@naver.com>
2023-10-07 15:42:34 +0900

    feat: 딜러의 기본 이름을 딜러로 설정

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -2,16 +2,18 @@ package domain;
 
 public class Dealer extends Player {
 
-    private final int STANDARD_OF_ADDITIONAl_CARD = 17;
+    private final static int STANDARD_OF_ADDITIONAl_CARD = 17;
+    private final static String DEFAULT_DEALER_NAME = "딜러";
 
-    private Dealer(Name name, Deck deck) {
-        super(name, deck);
+    private Dealer(Name name, Deck deck, Revenue revenue) {
+        super(name, deck, revenue);
     }
 
-    public static Dealer createDefaultDealer(final String name) {
+    public static Dealer createDefaultDealer() {
         Deck initialDeck = Deck.createDefaultDeck();
-        Name initialName = new Name(name);
-        return new Dealer(initialName, initialDeck);
+        Name initialName = new Name(DEFAULT_DEALER_NAME);
+        Revenue initialRevenue = Revenue.createDefaultRevenue();
+        return new Dealer(initialName, initialDeck,initialRevenue);
     }
 
     public boolean haveToPickMoreCard() {
