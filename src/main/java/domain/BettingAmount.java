df90660b78420ffd93d185302046a3943ba7ca85
WonyuChoi <phillip07290@naver.com>
2023-10-07 15:42:01 +0900

    feat: 배팅금을 반환하는 메서드 추가

--- src/main/java/domain/BettingAmount.java
+++ src/main/java/domain/BettingAmount.java
@@ -1,10 +1,13 @@
 package domain;
 
 public class BettingAmount {
-
     private final int bettingAmount;
 
     public BettingAmount(int bettingAmount) {
         this.bettingAmount=bettingAmount;
+    }
+
+    public int getBettingAmount() {
+        return bettingAmount;
     }
 }
