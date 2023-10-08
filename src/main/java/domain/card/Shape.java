6a56f952cce046e1e0bc5d6466c1659c265d6abf
WonyuChoi <phillip07290@naver.com>
2023-10-08 14:46:15 +0900

    feat: card의 모양을 담당하는 enum 객체 생성

---
+++ src/main/java/domain/card/Shape.java
@@ -0,0 +1,18 @@
+package domain.card;
+
+public enum Shape {
+    CLOVER("클로버"),
+    SPADE("스페이드"),
+    HEART("하트"),
+    DIAMOND("다이아몬드");
+
+    private String shape;
+
+    Shape(String shape) {
+        this.shape=shape;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+}
