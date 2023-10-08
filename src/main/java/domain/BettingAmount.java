ddd2437b2a4df6c2e737c43da7724e578ad2c7b1
WonyuChoi <phillip07290@naver.com>
2023-10-08 16:52:49 +0900

    feat: 배팅금 0보다 작으면 예외 처리

--- src/main/java/domain/BettingAmount.java
+++ src/main/java/domain/BettingAmount.java
@@ -4,8 +4,15 @@ public class BettingAmount {
 
     private final int bettingAmount;
 
-    public BettingAmount(int bettingAmount) {
+    public BettingAmount(final int bettingAmount) {
+        validateAmount(bettingAmount);
         this.bettingAmount = bettingAmount;
+    }
+
+    private void validateAmount(final int bettingAmount) {
+        if (bettingAmount <= 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getBettingAmount() {
